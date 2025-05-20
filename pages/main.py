1cfc962736b7a44349b80525b07e9c334d207d74
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-20 20:52:09 -0300

    casc

--- pages/main.py
+++ pages/main.py
@@ -85,15 +85,15 @@ def tela_login():
                 unsafe_allow_html=True
             )
 
-        st.markdown("""
-            <style>
-            #fake-button {display: none;}
-            </style>
-        """, unsafe_allow_html=True)
+            st.markdown("""
+                <style>
+                #fake-button {display: none;}
+                </style>
+            """, unsafe_allow_html=True)
 
-        if st.button("fake", key="fake-button"):
-            st.session_state.modo_recuperacao = True
-            st.experimental_rerun()
+            if st.button("fake", key="fake-button"):
+                st.session_state.modo_recuperacao = True
+                st.experimental_rerun()
 
         else:
             with st.form("form_esqueci"):
