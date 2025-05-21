ecf3c5f60083fedc8aac921e21c1fa974d600384
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-21 02:16:34 -0300

    cac

--- pages/main.py
+++ pages/main.py
@@ -263,6 +263,7 @@ def tela_login():
 # BLOQUEIA TUDO SE NÃO ESTIVER LOGADO
 if not st.session_state.usuario_logado:
     tela_login()
+
     st.title(st.session_state.pagina_atual)
     st.success(f"Bem-vindo, {st.session_state.nome}!")
     # aqui você pode chamar outras funções ou páginas, como tela_principal(), etc.
