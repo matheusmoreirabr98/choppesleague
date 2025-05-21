ddd9bfbdd571b11f42e7ea0b67b943db315bd6f7
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-20 21:00:57 -0300

    casc

--- pages/main.py
+++ pages/main.py
@@ -143,7 +143,7 @@ def tela_login():
     elif aba == "Cadastro":
         with st.form("form_cadastro"):
             nome = st.text_input("Nome completo", key="cad_nome")
-            posicao = st.selectbox("Posição que joga", ["", "Linha", "Goleiro"], key="cad_pos")
+            posicao = st.selectbox("Posição que joga", ["Linha", "Goleiro"], key="cad_pos")
             nascimento = st.date_input("Data de nascimento", value=date(2000, 1, 1), key="cad_nasc")
             telefone = st.text_input("Telefone (com DDD)", key="cad_tel")
             email = st.text_input("E-mail", key="cad_email")
