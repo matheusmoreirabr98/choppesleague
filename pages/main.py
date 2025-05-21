b3d08fad603e0183dcb326a4053a4899d4368b88
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-20 21:49:29 -0300

    xsx

--- pages/main.py
+++ pages/main.py
@@ -57,12 +57,10 @@ def tela_login():
                     <style>
                         div.stForm button[kind="primary"] {
                             display: block;
-                            margin-top: 1rem; auto 0 auto;
+                            margin: 1rem auto 0 auto;
                         }
                     </style>
                 """, unsafe_allow_html=True)
-
-                submit = st.form_submit_button("Entrar")
 
                 if submit:
                     usuarios = st.session_state.usuarios
