d66929fd2c803a2590f69487b9b4b11764e5236b
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-20 21:21:50 -0300

    cs

--- pages/main.py
+++ pages/main.py
@@ -143,8 +143,3 @@ def tela_login():
                     st.success("Cadastro realizado! Agora faça login.")
                     st.session_state.pagina_atual = "login"
                     st.experimental_rerun()
-
-# BLOQUEIA TUDO SE NÃO ESTIVER LOGADO
-if not st.session_state.usuario_logado:
-    tela_login()
-    st.stop()
