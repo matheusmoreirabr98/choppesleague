6b46f16206728833197f2d6c0f863aec3d0c8389
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-25 00:48:43 -0300

    xax

--- pages/main.py
+++ pages/main.py
@@ -541,7 +541,9 @@ else:
                     flex-wrap: nowrap;
                 ">
                     {escudo_borussia}
-                        <div style="font-size: 60px; font-weight: bold; line-height: 1;">X</div>
+                        <div style="font-size: 60px; font-weight: bold; line-height: 1;">
+                        ⚔️
+                        </div>
                     {escudo_inter}
                 </div>
             """, unsafe_allow_html=True)
