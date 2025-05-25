ea13ff51e05445539fe048eb0f8c21528d783994
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-25 02:47:29 -0300

    ca

--- pages/main.py
+++ pages/main.py
@@ -535,7 +535,7 @@ else:
     def tela_principal(partidas, jogadores):
         st.markdown("<h4 style='text-align: center; font-weight: bold;'>Bem-vindo à Choppe's League! 🍻</h4>", unsafe_allow_html=True)
         st.markdown("---")
-        st.markdown("<h3 style='text-align: center; font-weight: bold;'>.   Vitórias</h3>", unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: center; font-weight: bold;'>🏆 Vitórias</h3>", unsafe_allow_html=True)
 
         # Caminhos das imagens na pasta 'imagens'
         escudo_borussia = imagem_base64("imagens/escudo_borussia.png", "Borussia")
