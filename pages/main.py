4cee766aaded5b55d8e41806b065464996448b30
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-25 16:41:35 -0300

    acs

--- pages/main.py
+++ pages/main.py
@@ -446,7 +446,7 @@ else:
 
     # ✅ Tela principal com os escudos lado a lado e "X" no meio
     def tela_principal(partidas, jogadores):
-        st.markdown("<h2 style='text-align: center; font-weight: bold;'>Bem-vindo à Choppe's League! 🍻</h2>", unsafe_allow_html=True)
+        st.markdown("<h4 style='text-align: center; font-weight: bold;'>Bem-vindo à Choppe's League! 🍻</h4>", unsafe_allow_html=True)
         st.markdown("---")
 
         borussia_gols = 18
