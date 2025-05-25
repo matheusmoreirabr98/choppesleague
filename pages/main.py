17d4f495b7b7a7eeab494b6434c5bf1fa5ecffe4
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-25 00:42:11 -0300

    xa

--- pages/main.py
+++ pages/main.py
@@ -537,7 +537,7 @@ else:
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                    gap: 40px;
+                    gap: 70px;
                     flex-wrap: nowrap;
                 ">
                     {escudo_borussia}
