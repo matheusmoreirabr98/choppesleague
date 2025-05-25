ec7642093916edbfcc66235d0c914811b88e40db
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-25 16:09:55 -0300

    cs

--- pages/main.py
+++ pages/main.py
@@ -225,15 +225,14 @@ else:
     )
     st.session_state.pagina_atual = pagina_escolhida
     pagina_escolhida = st.selectbox(
-    "Selecione uma página:",
     opcoes,
     index=opcoes.index(st.session_state.pagina_atual),
     key="menu_topo"
 )
 
-if pagina_escolhida != st.session_state.pagina_atual:
-    st.session_state.pagina_atual = pagina_escolhida
-    st.rerun()
+    if pagina_escolhida != st.session_state.pagina_atual:
+        st.session_state.pagina_atual = pagina_escolhida
+        st.rerun()
 
         
 
