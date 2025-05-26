42d3f296a1483bb065dbf8a60e60d8bc457c1b13
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-26 20:33:11 -0300

    csd

--- pages/main.py
+++ pages/main.py
@@ -409,7 +409,7 @@ else:
 
     # --- SIDEBAR --- (imagem e nome apenas)
     with st.sidebar:
-        st.image("./imagens/logo.png", width=100)
+        st.image("./imagens/logo.png", width=150)
         st.markdown("<br>", unsafe_allow_html=True)
         st.markdown(f"👟 Jogador: **{st.session_state.nome}**")
 
