59790c2975686eb35260daaccf4d1557bc0efdc0
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-26 20:03:46 -0300

    casc

--- pages/main.py
+++ pages/main.py
@@ -409,7 +409,14 @@ else:
 
     # --- SIDEBAR --- (imagem e nome apenas)
     with st.sidebar:
-        st.image("./imagens/logo.png", width=150)
+        st.markdown(
+            """
+            <div style="text-align: center;">
+                <img src="./imagens/logo.png" width="150">
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
         st.markdown("<br>", unsafe_allow_html=True)
         st.markdown(f"👤 Jogador: **{st.session_state.nome}**")
 
