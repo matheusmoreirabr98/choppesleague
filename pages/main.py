f92e5f40c917f4d94e224bfb59bce45644fcdfec
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-26 20:17:37 -0300

    csac

--- pages/main.py
+++ pages/main.py
@@ -625,7 +625,7 @@ else:
             """
             <style>
                 [data-testid="stSidebar"] {
-                    width: 500px !important;
+                    width: 300px !important;
                 }
 
                 /* Corrige o conteúdo principal para não ser cortado */
