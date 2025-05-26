18140e2e6f538d91f12f9534dfec0f21c92cf930
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-26 20:26:12 -0300

    sax

--- pages/main.py
+++ pages/main.py
@@ -627,7 +627,7 @@ else:
 
                 /* Corrige o conteúdo principal para não ser cortado */
                 [data-testid="stSidebarContent"] {
-                    width: 220px !important;
+                    width: 250px !important;
                 }
             </style>
             """,
