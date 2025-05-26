4a55c7843a9756b5edae35f6cf6d8e287d389d52
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-26 20:15:39 -0300

    csc

--- pages/main.py
+++ pages/main.py
@@ -630,7 +630,7 @@ else:
 
                 /* Corrige o conteúdo principal para não ser cortado */
                 [data-testid="stSidebarContent"] {
-                    width: 200px !important;
+                    width: 80px !important;
                 }
             </style>
             """,
