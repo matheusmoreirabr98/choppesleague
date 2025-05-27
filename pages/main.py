c0c17f867ee8d3a72db3ba52e42aa656972ad80e
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-26 21:38:19 -0300

    vd

--- pages/main.py
+++ pages/main.py
@@ -440,7 +440,7 @@ else:
         ]
 
     # garante que sempre selecionamos uma opção válida da lista
-        pagina_ativa = st.session_state.pagina_atual
+    pagina_ativa = st.session_state.pagina_atual
 
     # exibe o selectbox somente se não estiver no Meu Perfil
     if pagina_ativa != "👤 Meu Perfil":
