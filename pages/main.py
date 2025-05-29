d5bdf7275ac4097c01d25f413776fee3609b87cd
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-28 23:57:27 -0300

    sc

--- pages/main.py
+++ pages/main.py
@@ -850,7 +850,7 @@ else:
                 </p>
             </div>
 
-            <div style="text-align: center; min-width: 10px;">
+            <div style="text-align: center; min-width: 13px;">
                 <p style="font-size: 25px;">
                     🤝<br>
                     {empates}
