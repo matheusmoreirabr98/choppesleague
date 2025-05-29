3f3a55a29dda1861b62bb478b36c83da5d225641
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-28 23:28:17 -0300

    cs

--- pages/main.py
+++ pages/main.py
@@ -970,53 +970,53 @@ else:
             st.markdown("<h5 style='text-align: center; font-weight: bold;'>Resultado da Partida</h5>",
             unsafe_allow_html=True,
             )
-            st.markdown(
-                f"""
-                    <div style="
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        gap: 50px;
-                        flex-wrap: nowrap;
-                    ">
-                        {escudo_borussia}
-                    <div style="font-size: 60px; font-weight: bold; line-height: 1;">⚔️
-                    </div>
-                        {escudo_inter}
-                    </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-            st.markdown(
-                f"""
+        st.markdown(
+            f"""
                 <div style="
                     display: flex;
-                    justify-content: space-between;
+                    justify-content: center;
                     align-items: center;
                     gap: 50px;
-                    margin-top: 20px;
-                    flex-wrap: wrap;
+                    flex-wrap: nowrap;
                 ">
-                <div style="text-align: right; min-width: 80px;">
-                    <p style="font-size: 30px;">
-                        {placar_borussia}
-                    </p>
+                    {escudo_borussia}
+                <div style="font-size: 60px; font-weight: bold; line-height: 1;">⚔️
                 </div>
-                <div style="text-align: center; min-width: 80px;">
-                    <p style="font-size: 30px;">
-                    </p>
-                </div>
-                <div style="text-align: left; min-width: 80px;">
-                    <p style="font-size: 30px;">
-                        {placar_inter}
-                    </p>
+                    {escudo_inter}
                 </div>
             """,
-                unsafe_allow_html=True,
-            )
+            unsafe_allow_html=True,
+        )
 
-            st.markdown("---")
+        st.markdown(
+            f"""
+            <div style="
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                gap: 50px;
+                margin-top: 20px;
+                flex-wrap: wrap;
+            ">
+            <div style="text-align: right; min-width: 80px;">
+                <p style="font-size: 30px;">
+                    {placar_borussia}
+                </p>
+            </div>
+            <div style="text-align: center; min-width: 80px;">
+                <p style="font-size: 30px;">
+                </p>
+            </div>
+            <div style="text-align: left; min-width: 80px;">
+                <p style="font-size: 30px;">
+                    {placar_inter}
+                </p>
+            </div>
+        """,
+            unsafe_allow_html=True,
+        )
+
+        st.markdown("---")
             
 
             if st.button("Registrar"):
