368f3789b67567258a36e55c7719e7657b6bc2ee
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-28 21:14:18 -0300

    csc

--- pages/main.py
+++ pages/main.py
@@ -217,7 +217,7 @@ def tela_login():
         "Escolha uma opção:", ["Login", "Cadastro"], key="aba_login", horizontal=True
     )
 
-    _, _, usuarios, presencas = load_data()  # ← lê os usuários direto da planilha
+    usuarios, presencas = load_data()  # ← lê os usuários direto da planilha
 
     # LOGIN
     if aba == "Login":
