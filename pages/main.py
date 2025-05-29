a8d0cc752c1517067e959e39a73d8fbbafe2d12e
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-28 23:36:55 -0300

    cs

--- pages/main.py
+++ pages/main.py
@@ -999,7 +999,7 @@ else:
                 margin-top: 20px;
                 flex-wrap: wrap;
             ">
-            <div style="text-align: right; min-width: 100px;">
+            <div style="text-align: right; min-width: 50px;">
                 <p style="font-size: 30px;">
                     {placar_borussia}
                 </p>
@@ -1008,7 +1008,7 @@ else:
                 <p style="font-size: 30px;">
                 </p>
             </div>
-            <div style="text-align: left; min-width: 10px;">
+            <div style="text-align: left; min-width: 50px;">
                 <p style="font-size: 30px;">
                     {placar_inter}
                 </p>
