90a308b1a131641c0fff6cbf25e4cfdab6b55d62
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2025-05-28 23:41:38 -0300

    cas

--- pages/main.py
+++ pages/main.py
@@ -1004,7 +1004,7 @@ else:
                     {placar_borussia}
                 </p>
             </div>
-            <div style="text-align: center; min-width: 200px;">
+            <div style="text-align: center; min-width: 100px;">
                 <p style="font-size: 30px;">
                 </p>
             </div>
